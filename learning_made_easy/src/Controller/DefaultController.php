8db8cbfabf125d073f43d8215b806020044c2c2d
Jack <jack@3sidedcube.com>
2020-03-05 09:16:01 +0000

    added

--- learning_made_easy/src/Controller/DefaultController.php
+++ learning_made_easy/src/Controller/DefaultController.php
@@ -3,17 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    /**
-     * @Route("/default", name="default")
-     */
+
     public function index()
     {
         return $this->json([
-            'message' => 'Welcome To Easy Learn',
+            'message' => 'Welcome to your new controller!',
         ]);
     }
 }
